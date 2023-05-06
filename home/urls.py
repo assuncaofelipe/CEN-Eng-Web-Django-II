3da1e636433bcf3e25dcd1ca430227dd4288d791
Felipe V Assunção <fassuncao368@gmail.com>
2023-05-06 12:40:14 -0400

    home

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', views.Home, name='login'),
     ]
