13c1b242c6fb00e52c6582396226e71a166a4a0d
Luscarvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-05-10 15:18:35 -0400

    :wrench: config: reset settings

--- core/settings.py
+++ core/settings.py
@@ -25,10 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #aplicação
-    'app',
-    'home',
-    'user'
 ]
 
 MIDDLEWARE = [
