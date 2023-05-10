930630b5fbb23c4440bffb714bb25f2d0194f87b
Luscarvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-05-10 16:26:53 -0400

    feat: crud equipamento

--- equipamento/models.py
+++ equipamento/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Equipamento(models.Model):
+    id_equipamento = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    patrimonio = models.CharField(unique=True, max_length=50)
--- equipamento/views.py
+++ equipamento/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+from .models import Equipamento
 
-# Create your views here.
+
+# @method_decorator(login_required, name='dispatch')
+class ListarEquipamento(ListView):
+    template_name = 'listar_equipamento.html'
+    model = Equipamento
+    context_object_name = 'equipamentos'
+
+
+# @method_decorator(login_required, name='dispatch')
+class CadastrarEquipamento(CreateView):
+    template_name = 'crud_equipamento.html'
+    model = Equipamento
+    fields = ['nome', 'patrimonio']
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Cadastrar"
+        return context
+
+
+# @method_decorator(login_required, name='dispatch')
+class EditarEquipamento(UpdateView):
+    template_name = 'crud_equipamento.html'
+    model = Equipamento
+    fields = ['nome', 'patrimonio']
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Editar"
+        return context
