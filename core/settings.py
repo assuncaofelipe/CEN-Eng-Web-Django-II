f98ed6b14fc3341e1434a650dc78db4f9fda76c9
Luscarvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-05-15 11:55:00 -0400

    🔧 config: crud emprestimo

--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'equipamento',
+    'emprestimo',
 ]
 
 MIDDLEWARE = [
@@ -94,13 +95,13 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Manaus'
 USE_I18N = True
-USE_L10N = True 
+USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/' 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
