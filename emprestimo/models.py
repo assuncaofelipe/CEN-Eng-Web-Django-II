70daa955b7061a62504e588aa6b4c348e197192e
Luscarvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-06-07 14:18:21 -0400

    feat: Crud emprestimo.

--- emprestimo/models.py
+++ emprestimo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from equipamento.models import Equipamento
 
-# Create your models here.
+
+class Emprestimo(models.Model):
+    id_emprestimo = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=250)
+    matricula = models.IntegerField(unique=True)
+    curso = models.CharField(max_length=250)
+    equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE)
+    data_emprestimo = models.DateTimeField('Inicio')
+    data_devolucao = models.DateTimeField('Fim')
+    observacao = models.CharField(max_length=500)
