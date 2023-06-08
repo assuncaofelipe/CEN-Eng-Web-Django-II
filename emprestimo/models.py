79e49c6b8f0b1a247261486f83096d3a6987c7e2
Luscarvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-06-07 21:32:15 -0400

    feat: crud emprestimo completo

--- emprestimo/models.py
+++ emprestimo/models.py
@@ -3,11 +3,20 @@ from equipamento.models import Equipamento
 
 
 class Emprestimo(models.Model):
+    curso_opcao = [
+        ("0", "Engenharia de Software"),
+        ("1", "Sistemas de Informação"),
+        ("2", "Engenharia de Produção"),
+        ("3", "Engenharia Sanitária"),
+        ("4", "Farmarcia"),
+        ("5", "Agronomia"),
+    ]
     id_emprestimo = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=250)
     matricula = models.IntegerField(unique=True)
-    curso = models.CharField(max_length=250)
+    curso = models.CharField(
+        choices=curso_opcao, default="0", max_length=2)
     equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE)
-    data_emprestimo = models.DateTimeField('Inicio')
-    data_devolucao = models.DateTimeField('Fim')
+    data_emprestimo = models.DateField('Inicio')
+    data_devolucao = models.DateField('Fim')
     observacao = models.CharField(max_length=500)
