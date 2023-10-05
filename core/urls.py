39738413d8eb180d178756856493bdbd33f20849
Felipe V Assunção <fassuncao368@gmail.com>
2023-10-04 23:18:24 -0400

    resolve login

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('login/', include('user.urls')),
     path('equipamento/', include('equipamento.urls')),
     path('emprestimo/', include('emprestimo.urls')),
 ]
