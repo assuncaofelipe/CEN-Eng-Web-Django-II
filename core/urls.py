aacc7a17ed3a076bb0c04b7cd5fb534288600737
Felipe de Vasconcelos Assunção <felipe.assuncao@eldorado.org.br>
2023-10-11 01:17:45 -0400

    ajustes de design

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", include("app.urls")),
     path("accounts/", include("user.urls")),
     path("admin/", admin.site.urls),
-    path("equipamentos/", include("equipamento.urls")),
+    path("equipamento/", include("equipamento.urls")),
     path("emprestimo/", include("emprestimo.urls")),
 ]
