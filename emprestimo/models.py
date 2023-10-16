e9ae83f3e388e9918daa78e578aabbf5741d0ee1
Felipe de Vasconcelos Assunção <felipe.assuncao@eldorado.org.br>
2023-10-16 17:58:54 -0400

    done

--- emprestimo/models.py
+++ emprestimo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from equipamento.models import Equipamento
+from django.utils import timezone
 
 
 class Emprestimo(models.Model):
@@ -16,18 +17,15 @@ class Emprestimo(models.Model):
         ("0", "Engenharia de Software"),
         ("1", "Sistemas de Informação"),
         ("2", "Engenharia de Produção"),
-        ("3", "Engenharia Sanitária"),
-        ("4", "Farmarcia"),
-        ("5", "Agronomia"),
     ]
     id_emprestimo = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=250)
-    matricula = models.IntegerField(unique=True)
-    curso = models.CharField(choices=curso_opcao, default="0", max_length=2)
-    equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE)
-    data_emprestimo = models.DateField("Inicio")
-    data_devolucao = models.DateField("Fim")
-    observacao = models.CharField(max_length=500, blank=True)
+    nome = models.CharField(max_length=250, verbose_name="Responsável")
+    matricula = models.IntegerField(unique=True, verbose_name="Matrícula")
+    curso = models.CharField(choices=curso_opcao, default="0", max_length=2, verbose_name="Curso")
+    equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE, verbose_name="Equipamento")
+    data_emprestimo = models.DateField(verbose_name="Data de Empréstimo", default=timezone.now)
+    data_devolucao = models.DateField(verbose_name="Data de DEvolução")
+    observacao = models.CharField(max_length=500, blank=True, verbose_name="Observações")
     status_emprestimo = models.CharField(max_length=1, choices=STATUS_EMPRESTIMO_CHOICES, default=EM_ANDAMENTO)
 
     def status_emprestimo_legivel(self):
