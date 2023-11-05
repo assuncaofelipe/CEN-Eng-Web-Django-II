6f292b775ecb49c99ade2de027a2266feae1472a
Felipe V Assunção <fassuncao368@gmail.com>
2023-11-05 10:19:30 -0400

    new ui-ux

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
-  const navbarMenu = document.querySelector(".navbar-menu");
 
   menuToggle.addEventListener("click", function () {
     navbarMenu.classList.toggle("active");
-  });
-
-  const navbarLinks = document.querySelectorAll(".navbar-links a");
-  const currentURL = window.location.pathname;
-
-  navbarLinks.forEach(link => {
-      const linkURL = link.getAttribute("href");
-
-      if (currentURL === linkURL) {
-          link.classList.add("active");
-      }
   });
 });
